ed7ca4bc9e5e849a687747f9498c7304646d0e48
TREY <rkh.mymail@gmail.com>
2014-12-12 15:34:37 -0500

    implemented authentication

--- app.rb
+++ app.rb
@@ -4,6 +4,62 @@ require 'sinatra/activerecord'
 
 require_relative  'config/environments.rb'
 
+require_relative "./models/user"
+require_relative "./models/gift"
+
+enable :sessions
+
+before do
+	session[:user] ||= []
+	path_no_base = request.url.gsub(request.base_url, '')
+  if session[:user].empty? && path_no_base != "/login" && path_no_base != "/signup"
+    redirect "/login"
+  end
+end
+
 get '/' do
 	return erb :index
+end
+
+get '/login' do
+	return erb :login
+end
+
+post "/login" do
+  email = params[:email]
+  password = params[:password]
+
+  user = User.find_by(email: email)
+  user_exists = (user != nil)
+  
+  if user_exists && user.authenticate(password)
+    session[:user] = {id: user.id, email: email}
+    redirect "/"
+  end
+  
+  redirect "/login"
+end
+
+get "/logout" do
+  session.clear
+  redirect '/'
+end
+
+post "/signup" do
+  email = params[:email]
+  password = params[:password]
+
+  puts "HERE #{params.inspect}"
+
+  user = User.new(email: email, password: password)
+
+  if user.save
+  	session[:user] = {id: user.id, email: email}
+ 		redirect "/"
+  else
+  	@errors = user.errors.full_messages
+  	puts @errors
+  end
+
+  
 end
--- models/user.rb
+++ models/user.rb
@@ -1,8 +1,43 @@
+require "bcrypt"
+# require_relative "../db/connection"
+
 class User < ActiveRecord::Base
-	validates_presence_of :email
-	validates_presence_of :password
-	
+	# attributes
+  attr_accessor :password, :password_confirmation
+
+  # callbacks
+  before_save :encrypt_password
+
+	# validations
+  # validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+  validates :email,
+    :presence => true,
+    :length => { maximum: 255 }
+  validates :email,
+    :presence => true,
+    :uniqueness => { case_sensitive: false },
+    :length => { maximum: 255 },
+    :format => { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+	# relationships
 	has_one :gift
 	has_one :secret_santee, class_name: "User", foreign_key: "secret_santee_id"
 	belongs_to :secret_santa, class_name: "User", foreign_key: "secret_santa_id"
+
+	# methods
+  def authenticate(password)
+    if BCrypt::Password.new(self.password_digest) == password
+      return true
+    else
+      return false
+    end
+  end
+
+  def encrypt_password
+    if password.present?
+      return self.password_digest = BCrypt::Password.create(password)
+    end
+  end
+
 end
